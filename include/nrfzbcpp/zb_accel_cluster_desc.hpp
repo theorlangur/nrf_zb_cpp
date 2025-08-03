98483a90303457039b8b41320892dc41cd1c4e3d
TheOrlangur <theorlangur@gmail.com>
2025-08-03 14:57:47 +0200

    added an event command for acceleration server cluster

--- include/nrfzbcpp/zb_accel_cluster_desc.hpp
+++ include/nrfzbcpp/zb_accel_cluster_desc.hpp
@@ -13,11 +13,30 @@ extern "C"
 namespace zb
 {
     static constexpr uint16_t kZB_ZCL_CLUSTER_ID_ACCEL = 0xfc00;
+    static constexpr uint8_t kZB_ZCL_ACCEL_CMD_ON_EVENT = 100;
+    struct EventArgs
+    {
+        uint32_t tap : 1;
+        uint32_t dbl_tap : 1;
+        uint32_t sixD_x_hi : 1;
+        uint32_t sixD_x_lo : 1;
+        uint32_t sixD_y_hi : 1;
+        uint32_t sixD_y_lo : 1;
+        uint32_t sixD_z_hi : 1;
+        uint32_t sixD_z_lo : 1;
+        uint32_t wake : 1;
+        uint32_t sleep : 1;
+        uint32_t flip_x : 1;
+        uint32_t flip_y : 1;
+        uint32_t flip_z : 1;
+    };
+
     struct zb_zcl_accel_basic_t
     {
         float x;//1.f == 1G
         float y;//1.f == 1G
         float z;//1.f == 1G
+        [[no_unique_address]]cluster_std_cmd_desc_t<kZB_ZCL_ACCEL_CMD_ON_EVENT, EventArgs> on_event;
     };
 
     template<> constexpr auto get_cluster_description<zb_zcl_accel_basic_t>()
@@ -29,6 +48,9 @@ namespace zb
                 cluster_mem_desc_t{.m = &T::x,.id = 0x0000, .a=Access::RP},
                 cluster_mem_desc_t{.m = &T::y,.id = 0x0001, .a=Access::RP},
                 cluster_mem_desc_t{.m = &T::z,.id = 0x0002, .a=Access::RP}
+            >{},
+            cluster_commands_desc_t<
+                 &T::on_event
             >{}
         >{};
     }
